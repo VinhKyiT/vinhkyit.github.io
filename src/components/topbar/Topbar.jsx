3a0a8e54560b3d42821fbd85f4132eb40934451a
Bùi Phạm Vĩnh Kỳ <vinhkyit2905@gmail.com>
2021-07-12 17:46:44 +0700

    upload

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,31 @@
+import "./topbar.scss";
+import { Person, Mail } from "@material-ui/icons";
+
+export default function Topbar({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            VinhKyIT.
+          </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>+84 3783 83 986</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>vinhkyit2905@gmail.com</span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
